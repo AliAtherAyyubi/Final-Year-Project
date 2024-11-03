02200787633c8133c47cff9dc6a3b9c5e644dc86
AliAtherAyyubi <info2atherayyubi@gmail.com>
2024-11-03 11:42:37 +0500

    updated

--- e_voting/lib/Models/owner.dart
+++
@@ -1,9 +0,0 @@
-class OwnerModel {
-  String? userID;
-  // String? ownerID;
-  String? orgID;
-  List<String>? candidates;
-  List<String>? voters;
-
-  OwnerModel({this.userID, this.orgID, this.candidates, this.voters});
-}
--- e_voting/lib/Services/textControl.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TextController {
-  TextEditingController email = TextEditingController();
-  TextEditingController name = TextEditingController();
-  TextEditingController cnic = TextEditingController();
-  TextEditingController password = TextEditingController();
-  TextEditingController confirmPassword = TextEditingController();
-}
